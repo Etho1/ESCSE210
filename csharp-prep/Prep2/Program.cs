bc33a9fbaa95f0c69e1130be2cfe58fae5578ac6
Ethan Spencer <ethan101@byui.edu>
2023-04-23 16:08:33 -0600

    Week 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter ="";
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+
+        float g = float.Parse(grade);
+        if (g >= 90)
+        {
+            letter = "A";
+        }
+        else if (g >= 80)
+        {
+            letter ="B";
+        }
+        else if (g >= 70)
+        {
+            letter ="C";
+        }
+        else if (g >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"{letter}");
+        if (g >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
     }
 }
