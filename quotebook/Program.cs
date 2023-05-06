fe10737ac4711911f6d150966ac8647a99b857d0
Ethan Spencer <ethan101@byui.edu>
2023-05-05 22:58:01 -0600

    follow along

---
+++ quotebook/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Source source = new Source("None were with Him","url");
+// Console.WriteLine(source.Stringify());
+
+Quote holland = new Quote("Elder Holland","Because Jesus.....",source);
+// Console.WriteLine(holland.GetQuote());
+
+Board quoteboard = new Board();
+quoteboard.AddQuote(holland);
+quoteboard.FindQuotesByAuthor("holland");
+
+Menu _menu = new Menu(quoteboard);
+_menu.Display();
